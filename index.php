0e0c50d7a774e0cc480d36b759117b2fb9efd073
GatsbyJr <61660956+goclfc@users.noreply.github.com>
2021-12-20 10:31:05 +0400

    tryin to get data from server
    aa

--- index.php
+++ index.php
@@ -11,5 +11,21 @@
     <header> 
     <h1> Hello World! </h1>
     </header>
+    <form action="#"> 
+    <input type="text" name="name" placeholder="Enter movie Name">
+    <button type="submit" id="submit">Submit</button>
+
+    </form>
+    <?php
+    $api_url = 'http://dummy.restapiexample.com/api/v1/employees';
+
+// Read JSON file
+$json_data = file_get_contents($api_url);
+
+// Decode JSON data into PHP array
+$response_data = json_decode($json_data);
+
+print_r ($response_data['0']);
+?>
 </body>
 </html>
